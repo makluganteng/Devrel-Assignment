17e1a5c739a873ef4e73214cfc792bc8a49e5123
V.O.T <makluganteng11@gmail.com>
2023-10-18 14:21:00 +0800

    write comments

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -16,28 +16,35 @@ export default function Home() {
   const [token, setToken] = useState<string>("");
   const [walletAddress, setWalletAddress] = useState<string>("");
 
+  //allow to get the address of the current connected wallet
   const { address } = useAccount();
+
+  //This is a react hook that allows to read a contract based on the contract address given and the ABI taht match the contract
   const { data } = useContractRead({
-    address: "0x7cb9f7fFDe47abbeF7A905151951fEF561a2f644",
-    abi: ABI,
-    functionName: "name",
-    args: [],
+    address: "0x7cb9f7fFDe47abbeF7A905151951fEF561a2f644", //contact address
+    abi: ABI, //ABI of the contract
+    functionName: "name", //function name
+    args: [], //arguments of the function
   });
 
+  //This is the function to handle the click to read
   const handleRead = () => {
     console.log(data);
     setToken(data as string);
   };
 
+  //This is a react hook that allows to prepare a write statement on a contract based on the contract address given and the ABI taht match the contract
   const { config } = usePrepareContractWrite({
-    address: "0x7cb9f7fFDe47abbeF7A905151951fEF561a2f644",
-    abi: ABI,
-    functionName: "safeMint",
-    args: [walletAddress],
+    address: "0x7cb9f7fFDe47abbeF7A905151951fEF561a2f644", //contact address
+    abi: ABI, //ABI of the contract
+    functionName: "safeMint", //function name
+    args: [walletAddress], //arguments of the function
   });
 
+  //This is a react hook that allows to write on a contract based on the contract address given and the ABI taht match the contract
   const { data: data2, isSuccess, isLoading, write } = useContractWrite(config);
 
+  //This is the function to handle the click to write
   const handleWrite = () => {
     if (write) write();
   };
